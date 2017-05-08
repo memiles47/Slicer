f1d9560809d90ea752be8fb1594975b2b7d0bfe0
Michael E Miles <michael@miles-me.net>
2017-05-08 12:45:32 -0500

    Initial commit

--- Slicer/Program.cs
+++ Slicer/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Slicer
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            //Get user email address
+            Console.Write("What is your email address?: ");
+            var email = Console.ReadLine();
+
+            //Slice out user name
+            var user = email.Substring(0, email.IndexOf("@"));
+
+            //Slice out domain name
+            var domain = email.Substring(email.IndexOf("@") + 1);
+
+            //Format message
+            var output = $"Your user name is {user} and your domain name is {domain}";
+
+            //Pring user namd and domain
+            Console.WriteLine(output);
         }
     }
 }
